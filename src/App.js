d4390ab8a47b7c2351fff5e656f540930b251a27
dead_boolean <34863781+jenil123@users.noreply.github.com>
2021-03-10 19:21:19 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Login from './Login/Login'
+import Editor from './Editor/Editor'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+//func
 function App() {
+  // const [name, setName]=useState('')
+  // const [room, setRoom]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route path='/' exact component={Login}></Route>
+      <Route path='/room' exact component={Editor}></Route>
+    </Router>
+  )
 }
 
-export default App;
+export default App
